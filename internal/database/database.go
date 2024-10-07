0ea3a710043589b29d0a9e333b332271887875fd
Murilo Alves <hi@omurilo.dev>
2024-10-07 17:13:01 -0300

    fix: add env vars to redis url, port and db

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,16 +2,21 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net"
+	"net/url"
 	"os"
+	"strconv"
 
 	redis "github.com/redis/go-redis/v9"
 )
 
 func NewDbClient() *redis.Client {
+	addr, db := getHostPortWithDefaults()
 	redisClient := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("REDIS_URL"),
-		DB:   0,
+		Addr: addr,
+		DB:   db,
 	})
 
 	if err := redisClient.Ping(context.Background()).Err(); err != nil {
@@ -19,4 +24,43 @@ func NewDbClient() *redis.Client {
 	}
 
 	return redisClient
+}
+
+func getEnv(key string, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getHostPortWithDefaults() (string, int) {
+	redisUrl := getEnv("REDIS_URL", "localhost:6379")
+	u, err := url.Parse(redisUrl)
+	if err != nil {
+    return "localhost:6379", 0
+	}
+
+	host, port, err := net.SplitHostPort(u.Host)
+	if err != nil {
+		host = u.Host
+		port = getEnv("REDIS_PORT", "6379")
+	}
+
+	if host == "" {
+		host = "localhost"
+	}
+
+	redisDB := u.Path
+	if redisDB == "" {
+		redisDB = fmt.Sprintf("/%s", getEnv("REDIS_DB", "0"))
+	}
+
+	db, err := strconv.Atoi(redisDB[1:])
+	if err != nil {
+		db = 0
+	}
+
+  addr := net.JoinHostPort(host, port)
+
+	return addr, db
 }
