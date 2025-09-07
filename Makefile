5ec0096bf8629236fc1d7f4446ec803c4536c228
Murilo Alves <hi@omurilo.dev>
2025-09-07 12:34:45 -0300

    feat: use embed fs to load template files
    add usage of goreleaser to release the project

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ docker-db:
 		: ; \
 	else \
 		echo "Falling back to Docker Compose V1"; \
-		docker-compose up redis; \
+		docker compose up redis; \
 	fi
 
 # Create DB container
@@ -26,7 +26,7 @@ docker-run:
 		: ; \
 	else \
 		echo "Falling back to Docker Compose V1"; \
-		docker-compose up --build; \
+		docker compose up --build; \
 	fi
 
 # Shutdown DB container
@@ -35,7 +35,7 @@ docker-down:
 		: ; \
 	else \
 		echo "Falling back to Docker Compose V1"; \
-		docker-compose down; \
+		docker compose down; \
 	fi
 
 # Clean up build artifacts
--- web/main.go
+++ web/main.go
@@ -1,26 +1,43 @@
 package web
 
 import (
+	"embed"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+//go:embed templates/*.html
+var templates embed.FS
+
 func Shareless(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("web/templates/index.html")
-	tmpl.Execute(w, nil)
+	tmpl, _:= template.ParseFS(templates, "templates/index.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func Shared(w http.ResponseWriter, shared interface{}) {
-	tmpl, _ := template.ParseFiles("web/templates/shared.html")
-	tmpl.Execute(w, shared)
+	tmpl, _ := template.ParseFS(templates, "templates/shared.html")
+	err := tmpl.Execute(w, shared)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("web/templates/about.html")
-	tmpl.Execute(w, nil)
+	tmpl, _ := template.ParseFS(templates, "templates/about.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func Privacy(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("web/templates/privacy.html")
-	tmpl.Execute(w, nil)
+	tmpl, _ := template.ParseFS(templates, "templates/privacy.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
